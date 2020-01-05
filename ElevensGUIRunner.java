8b0ca5ee6171ad11c8375d89b334b10c2703645f
yccgabby <59523545+yccgabby@users.noreply.github.com>
2020-01-05 04:42:12 -0500

    Add files via upload

---
+++ ElevensGUIRunner.java
@@ -0,0 +1,23 @@
+/*
+ * Gabby Chan
+ * Ms.Krasteva
+ * May 10th 2017
+ * Activity 9 code, plays the GUI version of the Elevens game.
+ */
+
+/**
+ * This is a class that plays the GUI version of the Elevens game.
+ * See accompanying documents for a description of how Elevens is played.
+ */
+public class ElevensGUIRunner {
+
+    /**
+     * Plays the GUI version of Elevens.
+     * @param args is not used.
+     */
+    public static void main(String[] args) {
+        Board board = new ElevensBoard();
+        CardGameGUI gui = new CardGameGUI(board);
+        gui.displayGame();
+    }
+}
